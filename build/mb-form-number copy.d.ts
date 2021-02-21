7b8dceb3e97554326c7e6d59e599b19474430258
mbenzekri <benzekri.mohamed@gmail.com>
2021-02-21 23:21:05 +0100

    removing unused files

--- build/mb-form-number copy.d.ts
+++
@@ -1,6 +0,0 @@
-import { MBFormField } from "./mb-form-field";
-export declare class MBFormNumber extends MBFormField {
-    constructor();
-    renderField(): import("lit-element").TemplateResult;
-}
-//# sourceMappingURL=mb-form-number copy.d.ts.map
